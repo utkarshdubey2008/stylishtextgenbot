1a038523b3186f5bcf387e17f2ff6498bb1cd35c
UTKARSHBOTS <90956407+utkarsh212646@users.noreply.github.com>
2023-03-07 07:59:38 +0530

    Update forcesub.py

--- plugins/forcesub.py
+++ plugins/forcesub.py
@@ -1,9 +1,8 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-import telegram
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-# Replace YOUR_TOKEN_HERE with your bot token
-bot = telegram.Bot(token='YOUR_TOKEN_HERE')
+# Replace YOUR_API_ID and YOUR_API_HASH with your API ID and API Hash
+app = Client("my_bot", api_id=YOUR_API_ID, api_hash=YOUR_API_HASH)
 
 # Replace GROUP_OR_CHANNEL_USERNAME with the username of the group or channel you want to force join
 group_username = 'GROUP_OR_CHANNEL_USERNAME'
@@ -12,24 +11,32 @@ group_username = 'GROUP_OR_CHANNEL_USERNAME'
 invite_link = 'INVITE_LINK'
 
 # Define the /start command handler
-def start(update, context):
-    user = update.message.from_user
-    chat_id = update.message.chat_id
+@app.on_message(filters.command('start'))
+def start_handler(client, message):
+    user = message.from_user
+    chat_id = message.chat.id
 
     # Create an inline keyboard with a "Join Group" button
-    keyboard = [[InlineKeyboardButton("Join Group", url=t.me/alphabotupdates)]]
+    keyboard = [[InlineKeyboardButton("Join Group", url=invite_link)]]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     # Send a message to the user with the inline keyboard
-    bot.send_message(chat_id=chat_id, text=f"Hi {user.first_name}! Please join the group {group_username} to use this bot.", reply_markup=reply_markup)
+    client.send_message(chat_id=chat_id, text=f"Hi {user.first_name}! Please join the group {group_username} to use this bot.", reply_markup=reply_markup)
 
-# Add the /start command handler to the bot
-from telegram.ext import CommandHandler
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+# Define the callback query handler for the "Join Group" button
+@app.on_callback_query(filters.regex('^join_group$'))
+def join_group_handler(client, callback_query):
+    chat_id = callback_query.message.chat.id
+    user_id = callback_query.from_user.id
 
-# Start the bot
-from telegram.ext import Updater
-updater = Updater(token='YOUR_TOKEN_HERE', use_context=True)
-dispatcher = updater.dispatcher
-updater.start_polling()
+    # Check if the user is already a member of the group
+    is_member = client.get_chat_member(chat_id, user_id).status
+
+    if is_member == 'left':
+        # If the user has left the group, send a message with the invite link and ask them to join
+        client.send_message(chat_id=user_id, text=f"Please join the group {group_username} to use this bot.", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Group", url=invite_link)]]))
+    elif is_member == 'kicked':
+        # If the user has been kicked from the group, send a message informing them that they cannot use the bot
+        client.send_message(chat_id=user_id, text="Sorry, you have been kicked from the group and cannot use this bot.")
+    else:
+        # If the user is already a member of
