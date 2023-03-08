304dd3c2ded97501e3871cdac7d6613bbfc0d01c
UTKARSHBOTS <90956407+utkarsh212646@users.noreply.github.com>
2023-03-08 09:55:32 +0530

    Create help.py

---
+++ plugins/help.py
@@ -0,0 +1,3 @@
+# Define the help function to handle the /help command
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I Can't Do Anything Just Send Me Text I Will Make It Stylish.")
